2db10188c0661f049247d88d039a5340c8b4fa14
genos-ux <gabriel.kyeremateng.gk@gmail.com>
2024-05-26 01:26:39 +0100

    working with classes in CSharp

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace WorkingWIthClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bankA = new Bank(){Amount = 100};
+            Bank bankB = new Bank(){Amount = 150};
+            Bank bankC = new Bank(){Amount = 200};
+
+            bankA.getBalance();
+            bankB.getBalance();
+            bankC.getBalance();
+        }
+    }
+
+    class Bank
+    {
+        private double deposit;
+
+        public double Deposit
+        {
+            get{ return deposit; }
+            set {deposit = value;}
+        }
+        public void getBalance()
+        {
+            Console.WriteLine("The amount balance is {0:C}",this.Deposit);
+
+        }
+
+    }
+
+
+}
